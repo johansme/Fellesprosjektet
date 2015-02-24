5f41bec5f92b2b5e0564685b01d46560af923bd9
ottes1 <birkottesen@dhcp-070156.wlan.ntnu.no>
2015-02-24 13:37:29 +0100

    meir login shits

--- src/login/loginController.java
+++ src/login/loginController.java
@@ -1,5 +1,60 @@
 package login;
 
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+
 public class loginController {
 
+	loginManager lman = new loginManager();
+
+	
+	
+	@FXML TextField username;
+	@FXML TextField password;
+	@FXML TextField errorMessage;
+	
+	@FXML
+	private void login(){
+		// get username and password
+		String uname;
+		String pass;
+		uname = username.textProperty().getValue();
+		pass = password.textProperty().getValue();
+		
+		//check if username and password corresponds
+		if(lman.checkLogin(uname, pass)){
+			
+		 // its a match, proceeding to calendar
+			
+			System.out.println("goodie");
+			
+		}else{
+			
+			// no match, alert user.
+			errorMessage.textProperty().setValue("Wrong username or password, or maybe botch, i dont know:D");
+			
+		}
+			
+		}
+	@FXML
+	private void hidePassword(){
+		//for privacy reasons we have to hide the password from potential evil masterminds
+		String pass;
+		pass = password.textProperty().getValue();
+		System.out.println(pass);
+		String hider ="";
+		for (int i = 0; i< pass.length();i++){
+			hider += "*";
+		}
+		password.textProperty().set("hider");
+	
+	
+	}
+	
+	
+	
+	
+	
 }
