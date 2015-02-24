4ccee4c6d4cc5e1a4b119013e913293f02c4612c
Johannes Skjeggestad Meyer <johannes@skmeyer.com>
2015-02-24 15:41:50 +0100

    Added fields

--- src/calendarGUI/MonthViewController.java
+++ src/calendarGUI/MonthViewController.java
@@ -1,10 +1,14 @@
 package calendarGUI;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
 public class MonthViewController {
 	
 	@FXML private Label monthName;
+	
+	@FXML private Button previous;
+	@FXML private Button next;
 
 }
