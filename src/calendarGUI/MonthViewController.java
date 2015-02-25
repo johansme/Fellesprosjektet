ef3987d3b1a96dbdaa43f5f2b30b618150ccac94
Johannes Skjeggestad Meyer <johannes@skmeyer.com>
2015-02-25 09:25:30 +0100

    Added week labels

--- src/calendarGUI/MonthViewController.java
+++ src/calendarGUI/MonthViewController.java
@@ -10,5 +10,12 @@ public class MonthViewController {
 	
 	@FXML private Button previous;
 	@FXML private Button next;
+	
+	@FXML private Label week1;
+	@FXML private Label week2;
+	@FXML private Label week3;
+	@FXML private Label week4;
+	@FXML private Label week5;
+	@FXML private Label week6;
 
 }
