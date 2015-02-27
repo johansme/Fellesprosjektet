8f8634f56abc6918067066dfbc2a87a2b70d73e7
Johannes Skjeggestad Meyer <johannes@skmeyer.com>
2015-02-27 13:04:55 +0100

    Added basics for month handling

--- src/calendar/Calendar.java
+++ src/calendar/Calendar.java
@@ -1,15 +1,21 @@
 package calendar;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Calendar {
 	
 	//Current here means the date etc. to be shown in the calendar.
 	private LocalDate currentDate;
 	private Month currentMonth;
+	private List<Month> months;
 	
 	public Calendar() {
 		currentDate = LocalDate.now();
+		currentMonth = new Month(currentDate);
+		months = new ArrayList<Month>();
+		months.add(currentMonth);
 	}
 	
 	public LocalDate getCurrentDate() {
@@ -18,6 +24,47 @@ public class Calendar {
 	
 	public Month getCurrentMonth() {
 		return currentMonth;
+	}
+	
+	public List<Month> getMonths() {
+		return months;
+	}
+	
+	public Month getNextMonth() {
+		int i = months.indexOf(currentMonth);
+		if (i == months.size() - 1) {
+			Month month = new Month(currentDate.plusMonths(1));
+			months.add(month);
+			currentDate = currentDate.plusMonths(1);
+			currentDate.minusDays(currentDate.getDayOfMonth()-1);
+			currentMonth = month;
+			return month;
+		} else {
+			currentDate = currentDate.plusMonths(1);
+			currentDate.minusDays(currentDate.getDayOfMonth()-1);
+			currentMonth = months.get(i + 1);
+			return months.get(i + 1);
+		}
+	}
+	
+	public Month getPreviousMonth() throws IllegalStateException {
+		if (currentDate.isBefore(LocalDate.of(2000, 2, 1))) {
+			throw new IllegalStateException("This calendar does not support dates prior to the year 2000");
+		}
+		int i = months.indexOf(currentMonth);
+		if (i == 0) {
+			Month month = new Month(currentDate.minusMonths(1));
+			months.add(0, month);
+			currentDate = currentDate.minusMonths(1);
+			currentDate.plusDays(currentDate.lengthOfMonth()-currentDate.getDayOfMonth());
+			currentMonth = month;
+			return month;
+		} else {
+			currentDate = currentDate.minusMonths(1);
+			currentDate.plusDays(currentDate.lengthOfMonth()-currentDate.getDayOfMonth());
+			currentMonth = months.get(i - 1);
+			return months.get(i - 1);
+		}
 	}
 
 }
