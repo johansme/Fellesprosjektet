df095d24f976d2fc86da1dc6625e10e77abf65a3
Einar Hov <einhov@gmail.com>
2015-03-03 13:06:40 +0100

    HTTP Session system

---
+++ src/server/HttpSession.java
@@ -0,0 +1,23 @@
+package server;
+
+class HttpSession {
+	private int uid;
+	private long lastAccessed;
+	
+	public HttpSession(int uid) {
+		this.uid = uid;
+		touch();
+	}
+	
+	public void touch() {
+		lastAccessed = System.currentTimeMillis();
+	}
+
+	public int getUid() {
+		return uid;
+	}
+	
+	public long getLastAccessed() {
+		return lastAccessed;
+	}
+}
