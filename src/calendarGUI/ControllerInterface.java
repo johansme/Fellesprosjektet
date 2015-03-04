0318bcb744bbf10887b473b9b67abb33cebe3ec4
Alexander Bakken <alex.h.bakken@gmail.com>
2015-03-04 11:29:19 +0100

    alexx

---
+++ src/calendarGUI/ControllerInterface.java
@@ -0,0 +1,12 @@
+package calendarGUI;
+
+import calendar.Appointment;
+import calendar.Calendar;
+
+public interface ControllerInterface {
+
+	
+	public void setData(Calendar calendar);
+	public Calendar getData();
+	public void setData(Calendar c, Appointment a);
+}
