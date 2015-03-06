3357607de2785d3ab2f4dcc51f43836fcf4c63df
Einar Hov <einhov@gmail.com>
2015-03-06 12:12:19 +0100

    Add utility function for getting JSON from IS

---
+++ src/server/Util.java
@@ -0,0 +1,28 @@
+package server;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class Util {
+	public static JSONObject streamToJSON(InputStream is) throws IOException {
+		JSONObject obj;
+		StringBuilder sb;
+
+		int b;
+		sb = new StringBuilder();
+		while((b = is.read()) != -1) {
+			sb.append((char)b);
+		}
+		is.close();
+		
+		try {
+			obj = new JSONObject(sb.toString());
+		} catch(JSONException e) {
+			obj = null;
+		}
+		return obj;
+	}
+}
--- src/server/httpserver/HttpLogin.java
+++ src/server/httpserver/HttpLogin.java
@@ -1,7 +1,6 @@
 package server.httpserver;
 
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.OutputStream;
 
 import com.sun.net.httpserver.*;
@@ -9,6 +8,7 @@ import com.sun.net.httpserver.*;
 import org.json.*;
 
 import server.User;
+import server.Util;
 
 public class HttpLogin implements HttpHandler {
 	Server server;
@@ -18,36 +18,36 @@ public class HttpLogin implements HttpHandler {
 	}
 
 	public void handle(HttpExchange t) throws IOException {
-		final InputStream is;
-		StringBuilder sb = new StringBuilder();
+		JSONObject response = new JSONObject();
+		JSONObject obj = Util.streamToJSON(t.getRequestBody());
 
-		is = t.getRequestBody();
-
-		int b;
-		while((b = is.read()) != -1) {
-			sb.append((char)b);
-		}
-		is.close();
-
-		JSONObject obj = new JSONObject(sb.toString());
-		String username = (String)obj.get("username");
-		String password = (String)obj.get("password");
-
-		String response = new String();
-		if(username != null || password != null) {
-			if(User.checkPassword(username, password)) {
-				User u = new User(username);
-				response = (String)server.getSessionManager().getNewSession(u.getId());
+		if(obj != null) {
+			String username = (String)obj.get("username");
+			String password = (String)obj.get("password");
+		
+			if(obj != null && (username != null && password != null)) {
+				if(User.checkPassword(username, password)) {
+					User u = new User(username);
+					response.put("status", true);
+					response.put("session", (String)server.getSessionManager().getNewSession(u.getId()));
+				} else {
+					response.put("status", false);
+					response.put("error", "Invalid username or password");
+				}
 			} else {
-				response = "Invalid username or password";
+				response.put("status", false);
+				response.put("error", "Invalid request");
 			}
 		} else {
-			response = "Invalid request";
+			response.put("status", false);
+			response.put("error", "Not JSON");
 		}
-
-		t.sendResponseHeaders(200, response.length());
+		
+		String response_str = response.toString();
+		System.out.println(response_str);
+		t.sendResponseHeaders(200, response_str.length());
 		OutputStream out = t.getResponseBody();
-		out.write(response.getBytes());
+		out.write(response_str.getBytes());
 		t.close();
 	}
 }
