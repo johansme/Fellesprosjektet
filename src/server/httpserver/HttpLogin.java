455330a9b8e37dc892dd73e2c8b630fc1c40b0c1
Einar Hov <einhov@gmail.com>
2015-03-10 11:51:20 +0100

    Make a subclassed HttpHandler for API resources

--- src/server/httpserver/HttpLogin.java
+++ src/server/httpserver/HttpLogin.java
@@ -1,25 +1,21 @@
 package server.httpserver;
 
 import java.io.IOException;
-import java.io.OutputStream;
 
 import com.sun.net.httpserver.*;
 
 import org.json.*;
 
 import server.User;
-import server.Util;
 
-public class HttpLogin implements HttpHandler {
-	Server server;
-
+public class HttpLogin extends HttpAPIHandler {
 	public HttpLogin(Server server) {
-		this.server = server;
+		super(server);
 	}
 
-	public void handle(HttpExchange t) throws IOException {
+	public void handle(HttpExchange t)  throws IOException {
 		JSONObject response = new JSONObject();
-		JSONObject obj = Util.streamToJSON(t.getRequestBody());
+		JSONObject obj = get(t);
 
 		if(obj != null) {
 			String username = (String)obj.get("username");
@@ -44,11 +40,6 @@ public class HttpLogin implements HttpHandler {
 			response.put("error", "Not JSON");
 		}
 		
-		String response_str = response.toString();
-		System.out.println(response_str);
-		t.sendResponseHeaders(200, response_str.length());
-		OutputStream out = t.getResponseBody();
-		out.write(response_str.getBytes());
-		t.close();
+		send(t, response);
 	}
 }
