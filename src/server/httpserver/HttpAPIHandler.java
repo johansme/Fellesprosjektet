db0eb5c94e7329f425b59eff2bcfb6a2d757a659
Einar Hov <einhov@gmail.com>
2015-03-10 13:24:10 +0100

    Move streamToJSON to a shared Util class

--- src/server/httpserver/HttpAPIHandler.java
+++ src/server/httpserver/HttpAPIHandler.java
@@ -1,14 +1,14 @@
 package server.httpserver;
 
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.OutputStream;
 
-import org.json.JSONException;
 import org.json.JSONObject;
 
 import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpExchange;
+
+import shared.Util;
 
 public abstract class HttpAPIHandler implements HttpHandler{
 	protected Server server;
@@ -17,28 +17,8 @@ public abstract class HttpAPIHandler implements HttpHandler{
 		this.server = server;
 	}
 	
-	private static JSONObject streamToJSON(InputStream is) throws IOException {
-		JSONObject obj;
-		StringBuilder sb;
-
-		int b;
-		sb = new StringBuilder();
-		while((b = is.read()) != -1) {
-			sb.append((char)b);
-		}
-		is.close();
-		
-		try {
-			obj = new JSONObject(sb.toString());
-		} catch(JSONException e) {
-			obj = null;
-		}
-		return obj;
-	}
-
-	
 	static protected JSONObject get(HttpExchange t) throws IOException {
-		return streamToJSON(t.getRequestBody());
+		return Util.streamToJSON(t.getRequestBody());
 	}
 	
 	static protected void send(HttpExchange t, JSONObject response) throws IOException {
