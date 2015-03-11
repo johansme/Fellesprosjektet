792b54adf065cafd01011be06d865a97150a70b6
Johannes Skjeggestad Meyer <johannes@skmeyer.com>
2015-03-11 14:06:28 +0100

    Minor changes

--- src/calendarGUI/FilterViewController.java
+++ src/calendarGUI/FilterViewController.java
@@ -15,7 +15,7 @@ import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.layout.HBox;
 
-public class GroupViewController {
+public class FilterViewController {
 
 	@FXML
 	private ListView<HBox> groupList;
