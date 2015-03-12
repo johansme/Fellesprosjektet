e0136a5a640a3e9eb37ef6aea08a975ff6b4c9de
Einar Hov <einhov@gmail.com>
2015-03-12 17:31:18 +0100

    Add shared Group class

---
+++ src/shared/Group.java
@@ -0,0 +1,64 @@
+package shared;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class Group {
+	private int id;
+	private int parent;
+	private int createdBy;
+	private String name;
+	
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getParent() {
+		return parent;
+	}
+
+	public void setParent(int parent) {
+		this.parent = parent;
+	}
+
+	public int getCreatedBy() {
+		return createdBy;
+	}
+
+	public void setCreatedBy(int createdBy) {
+		this.createdBy = createdBy;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public JSONObject toJSON() {
+		JSONObject obj = new JSONObject();
+		try {
+			obj.put("id", id);
+			obj.put("name", name);
+			obj.put("parent", parent);
+			obj.put("createdby", createdBy);
+		} catch(JSONException e) { return null; }
+		return obj;
+	}
+
+	public boolean fromJSON(JSONObject obj) {
+		try {
+			id = obj.getInt("id");
+			name = obj.getString("name");
+			parent = obj.getInt("parent");
+			createdBy = obj.getInt("createdby");
+		} catch(JSONException e) { return false; }
+		return true;
+	}
+}
