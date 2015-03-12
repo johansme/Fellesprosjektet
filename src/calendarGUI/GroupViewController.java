c4cd99c678dc7575d8c892ddb96a41f73edf9a1c
Johannes Skjeggestad Meyer <johannes@skmeyer.com>
2015-03-12 10:04:07 +0100

    Updated controller

--- src/calendarGUI/GroupViewController.java
+++ src/calendarGUI/GroupViewController.java
@@ -1,12 +1,22 @@
 package calendarGUI;
 
+import calendar.Appointment;
+import calendar.Calendar;
+import calendar.Participant;
 import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.MenuButton;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
-public class GroupViewController extends Application {
+public class GroupViewController extends Application implements ControllerInterface {
 	
 	@Override
 	public void start(Stage primaryStage) {
@@ -23,6 +33,77 @@ public class GroupViewController extends Application {
 	
 	public static void main(String[] args) {
 		launch(args);
+	}
+	
+	@FXML private Tab newGroup;
+	@FXML private TextField nameFieldNew;
+	@FXML private Label creatorLabelNew;
+	@FXML private ListView<Participant> memberListNew;
+	@FXML private Button addMembersButtonNew;
+	@FXML private Button removeMemberButtonNew;
+	@FXML private Button saveButtonNew;
+	@FXML private Button cancelButtonNew;
+	
+	@FXML private Tab groups;
+	@FXML private MenuButton groupChoice;
+	@FXML private Label nameLabel;
+	@FXML private TextField nameField;
+	@FXML private Label creatorLabel;
+	@FXML private ListView<Participant> memberList;
+	@FXML private Button addMembersButton;
+	@FXML private Button removeMemberButton;
+	@FXML private Button saveButton;
+	@FXML private Button cancelButton;
+	
+	private Calendar calendar;
+	
+	@FXML
+	private void initialize() {
+		
+	}
+	
+	@FXML
+	private void addMembers() {
+		
+	}
+	
+	@FXML
+	private void removeMember() {
+		
+	}
+	
+	@FXML
+	private void saveButtonPressed() {
+		
+	}
+	
+	@FXML
+	private void cancelButtonPressed() {
+		
+	}
+
+	@Override
+	public void setData(Calendar calendar) {
+		if (calendar != null) {
+			this.calendar = calendar;
+		}
+	}
+
+	@Override
+	public Calendar getData() {
+		return calendar;
+	}
+
+	@Override
+	public void setData(Calendar c, Appointment a) {
+		if (c != null) {
+			calendar = c;
+		}
+	}
+
+	@Override
+	public void setFeedback() {
+		
 	}
 
 }
