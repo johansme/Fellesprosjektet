3829e6e9f2599678119791f6b7f717063c8523a1
ottes1 <birkottesen@dhcp-029062.wlan.ntnu.no>
2015-03-13 16:51:03 +0100

    hmm

--- src/login/SceneHandler.java
+++ src/login/SceneHandler.java
@@ -12,6 +12,7 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 public class SceneHandler {
