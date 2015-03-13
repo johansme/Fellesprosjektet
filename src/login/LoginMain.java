119131bc4b82266bc9ef7a23475eb1670acd5616
Johannes Skjeggestad Meyer <johannes@skmeyer.com>
2015-03-13 16:15:23 +0100

    Added tm

--- src/login/LoginMain.java
+++ src/login/LoginMain.java
@@ -18,7 +18,7 @@ public class LoginMain extends Application {
 			Scene scene = new Scene(root,307,258);
 			primaryStage.setScene(scene);
 			primaryStage.getIcons().add(new Image(this.getClass().getResourceAsStream("appointment_icon.png")));
-			primaryStage.titleProperty().setValue("BETJA Systemsâ„¢");
+			primaryStage.titleProperty().setValue("BETJA Systems™");
 			primaryStage.show();
 		} catch(Exception e) 
 		{
