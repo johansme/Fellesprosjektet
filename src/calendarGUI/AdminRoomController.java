9ebaf748badfd6c4db5fd65166f0581874493630
Johannes Skjeggestad Meyer <johannes@skmeyer.com>
2015-03-17 15:25:46 +0100

    Finished create new user

--- src/calendarGUI/AdminRoomController.java
+++ src/calendarGUI/AdminRoomController.java
@@ -11,7 +11,6 @@ import api.API;
 import newAppointment.Room;
 import calendar.Appointment;
 import calendar.Calendar;
-import calendar.User;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
@@ -37,22 +36,24 @@ public class AdminRoomController implements ControllerInterface {
 	private Calendar calendar;
 	private SceneHandler sceneHandler = new SceneHandler();
 
-	
-	
+
+
 	@FXML
 	private void addRoom(){
-		
 		if (isValidRoom()) {
 			Room room = new Room();
 			room.setName(roomNameField.getText());
 			room.setCapacity(Integer.valueOf(capacityField.getText()));
 			JSONObject obj = new JSONObject();
 			obj.put("command", "create");
-			obj.put("room", room);
+			obj.put("room", room.toJSON());
 			try {
 				JSONObject res = API.call("/room", obj, calendar.getSession());
 				room.setId(res.getInt("rid"));
+				roomList.add(room);
+				createRoomElement(room);
 			} catch (IOException e) {
+				sceneHandler.popUpMessage("/messages/Error.fxml", 290, 140, "Something went wrong. Please try again.", this);
 			}
 		} else {
 			sceneHandler.popUpMessage("/messages/Error.fxml", 290, 140, "Invalid room data", this);
@@ -73,52 +74,54 @@ public class AdminRoomController implements ControllerInterface {
 		if (! capacityField.getText().matches("[0-9]+") || capacityField.getText().length() > 5) {
 			return false;
 		}
-		if (! roomNameField.getText().matches("[a-zA-Z0-9 -_]+") || roomNameField.getText().length() > 50) {
+		if (! roomNameField.getText().matches("[a-zA-Z0-9 _\\-]+") || roomNameField.getText().length() > 17) {
 			return false;
 		}
 		return true;
 	}
-	
+
 	private void createRoomListView(){
 		if (roomList!=null && !roomList.isEmpty()) {
 			for (Room room : roomList) {
-				
-				HBox line = new HBox();
-				line.setPrefWidth((int)(roomListView.getPrefWidth()*0.90));
-				
-				
-				//label for the line:
-				Label userLabel = new Label();
-				userLabel.wrapTextProperty().set(true);
-				userLabel.setFocusTraversable(false);
-				userLabel.setText(room.getName() +" Capasity: " + room.getCapacity());
-				
-				//button to delete room:
-				Button deleteRoom = new Button();
-				deleteRoom.setPrefWidth((int)(roomListView.getPrefWidth()*0.1));
-				deleteRoom.setText("X");
-				deleteRoom.setTextFill(new Color(1, 0, 0, 1));
-				
-				
-				deleteRoom.setOnAction(new EventHandler<ActionEvent>() {
-					@Override public void handle(ActionEvent e) {
-						//TODO
-						// remove room from database :O
-						roomListView.getItems().remove(line);
-						
-					}
-				});
-				
-				userLabel.setPrefWidth(line.getPrefWidth()-deleteRoom.getPrefWidth());
-				line.getChildren().addAll(userLabel, deleteRoom);
-				line.setFocusTraversable(false);
-				
-				
-				
-				roomListView.getItems().add(line);
+				createRoomElement(room);
 			}
+		}
 	}
+	
+	private void createRoomElement(Room room) {
+		HBox line = new HBox();
+		line.setPrefWidth((int)(roomListView.getPrefWidth()*0.90));
 		
+		
+		//label for the line:
+		Label userLabel = new Label();
+		userLabel.wrapTextProperty().set(true);
+		userLabel.setFocusTraversable(false);
+		userLabel.setText(room.getName() +" Capasity: " + room.getCapacity());
+		
+		//button to delete room:
+		Button deleteRoom = new Button();
+		deleteRoom.setPrefWidth((int)(roomListView.getPrefWidth()*0.1));
+		deleteRoom.setText("X");
+		deleteRoom.setTextFill(new Color(1, 0, 0, 1));
+		
+		
+		deleteRoom.setOnAction(new EventHandler<ActionEvent>() {
+			@Override public void handle(ActionEvent e) {
+				//TODO
+				// remove room from database :O
+				roomListView.getItems().remove(line);
+				
+			}
+		});
+		
+		userLabel.setPrefWidth(line.getPrefWidth()-deleteRoom.getPrefWidth());
+		line.getChildren().addAll(userLabel, deleteRoom);
+		line.setFocusTraversable(false);
+		
+		
+		
+		roomListView.getItems().add(line);
 		
 	}
 
@@ -126,7 +129,7 @@ public class AdminRoomController implements ControllerInterface {
 		JSONObject obj = new JSONObject();
 		obj.put("command", "get_all");
 		try {
-			
+
 			JSONObject res = API.call("/rooms", obj, calendar.getSession());
 			JSONArray resArray = res.getJSONArray("rooms");
 			for (int i = 0; i < resArray.length(); i++) {
--- src/calendarGUI/AdminUserController.java
+++ src/calendarGUI/AdminUserController.java
@@ -2,7 +2,6 @@ package calendarGUI;
 
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 
 import org.json.JSONArray;
@@ -11,11 +10,11 @@ import org.json.JSONObject;
 import api.API;
 import calendar.Appointment;
 import calendar.Calendar;
-import calendar.Group;
 import calendar.User;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
@@ -36,22 +35,25 @@ public class AdminUserController implements ControllerInterface {
 
 	@FXML private Tab rooms;
 	@FXML private AdminRoomController roomsPaneController;
-	
+
 	@FXML private TabPane tabs;
 	@FXML private Tab users;
+	@FXML private TextField surnameField;
+	@FXML private TextField nameField;
+	@FXML private TextField emailField;
 	@FXML private TextField usernameField;
 	@FXML private PasswordField passwordField;
 	@FXML private CheckBox newAdmin;
 	@FXML private Button addUserButton;
 	@FXML private Button closeButton;
-
-	@FXML private ListView<User> userListView;
 	
+	private AdminUserController thisController = this;
+
 	@FXML
 	private ListView<HBox> userElementsList;
-	
+
 	private List<User> userList = new ArrayList<User>();
-	
+
 
 	private Calendar calendar;
 	private SceneHandler sceneHandler = new SceneHandler();
@@ -60,17 +62,36 @@ public class AdminUserController implements ControllerInterface {
 	private void addUser(){
 		if (isValidUser()) {
 			User user = new User();
+			user.setSurname(surnameField.getText());
+			user.setName(nameField.getText());
+			user.setEmail(emailField.getText());
 			user.setUsername(usernameField.getText());
 			user.setAdmin(newAdmin.isSelected());
-			//TODO
+			JSONObject obj = new JSONObject();
+			obj.put("command", "create");
+			obj.put("user", user.toJSON());
+			obj.put("password", passwordField.getText());
+			try {
+				API.call("/user", obj, calendar.getSession());
+				userList.add(user);
+				createUserElement(user);
+				surnameField.clear();
+				nameField.clear();
+				emailField.clear();
+				usernameField.clear();
+				passwordField.clear();
+				newAdmin.selectedProperty().set(false);
+			} catch (IOException e) {
+				sceneHandler.popUpMessage("/messages/Error.fxml", 290, 140, "Something went wrong. Please try again.", this);
+			}
 		} else {
 			sceneHandler.popUpMessage("/messages/Error.fxml", 290, 140, "Insufficient user info", this);
 		}
 	}
-	
+
 	@FXML
 	private void roomsPressed() {
-		
+
 	}
 
 	@FXML
@@ -79,68 +100,98 @@ public class AdminUserController implements ControllerInterface {
 		Stage stage = (Stage)closeButton.getScene().getWindow();
 		stage.close();
 	}
-	
+
 	private void createUserList(){
 		if (userList!=null && !userList.isEmpty()) {
 			for (User usr : userList) {
-				
-				HBox line = new HBox();
-				line.setPrefWidth((int)(userElementsList.getPrefWidth()*0.95));
-				
-				
-				//label for the line:
-				Label userLabel = new Label();
-				userLabel.wrapTextProperty().set(true);
-				userLabel.setFocusTraversable(false);
-				userLabel.setText(usr.getName() +" " + usr.getSurname());
-				
-				userLabel.setFont(new Font(18));
-				
-
-				
-				//button to change password:
-				Button changePassButton = new Button();
-				changePassButton.setPrefWidth((int)(userElementsList.getPrefWidth()*0.4));
-				changePassButton.setText("Change Password");
-				
-				
-				changePassButton.setOnAction(new EventHandler<ActionEvent>() {
-					@Override public void handle(ActionEvent e) {
-						// launch change password window for targeted user:
-						sceneHandler.popUpChangePassword("/calendarGUI/EditUserPasswordView.fxml", 240, 190, usr);
-					}
-				});
-				
-				//button to delete user:
-				Button deleteUser = new Button();
-				deleteUser.setPrefWidth((int)(userElementsList.getPrefWidth()*0.1));
-				deleteUser.setText("X");
-				deleteUser.setTextFill(new Color(1, 0, 0, 1));
-				
-				
-				deleteUser.setOnAction(new EventHandler<ActionEvent>() {
-					@Override public void handle(ActionEvent e) {
-						//TODO
-						// remove user from database :O
-						userElementsList.getItems().remove(line);
-						
-					}
-				});
-				
-				userLabel.setPrefWidth(line.getPrefWidth()- changePassButton.getPrefWidth()-deleteUser.getPrefWidth());
-				line.getChildren().addAll(userLabel, changePassButton,deleteUser);
-				line.setFocusTraversable(false);
-				
-				
-				
-				userElementsList.getItems().add(line);
+				createUserElement(usr);
 			}
-	}
+		}
 	}
 	
+	private void createUserElement(User user) {
+		HBox line = new HBox();
+		line.setPrefWidth((int)(userElementsList.getPrefWidth()*0.95));
+		line.alignmentProperty().set(Pos.CENTER);
+		
+		
+		//label for the line:
+		Label userLabel = new Label();
+		userLabel.wrapTextProperty().set(true);
+		userLabel.setFocusTraversable(false);
+		userLabel.setText(user.getName() +" " + user.getSurname() + ", " + user.getEmail());
+		
+		userLabel.setFont(new Font(14));
+		
+		
+		
+		//button to change password:
+		Button changePassButton = new Button();
+		changePassButton.setPrefWidth((int)(userElementsList.getPrefWidth()*0.4));
+		changePassButton.setText("Change Password");
+		
+		
+		changePassButton.setOnAction(new EventHandler<ActionEvent>() {
+			@Override public void handle(ActionEvent e) {
+				// launch change password window for targeted user:
+				sceneHandler.popUpChangePassword("/calendarGUI/EditUserPasswordView.fxml", 240, 190, user);
+			}
+		});
+		
+		//button to delete user:
+		Button deleteUser = new Button();
+		deleteUser.setPrefWidth((int)(userElementsList.getPrefWidth()*0.1));
+		deleteUser.setText("X");
+		deleteUser.setTextFill(new Color(1, 0, 0, 1));
+		
+		
+		deleteUser.setOnAction(new EventHandler<ActionEvent>() {
+			@Override public void handle(ActionEvent e) {
+				//TODO
+				delLine = line;
+				sceneHandler.popUpMessage("/messages/Confirm.fxml", 290, 140, "Are you sure you want to delete?", thisController);
+			}
+		});
+		
+		userLabel.setPrefWidth(line.getPrefWidth()- changePassButton.getPrefWidth()-deleteUser.getPrefWidth());
+		line.getChildren().addAll(userLabel, changePassButton,deleteUser);
+		line.setFocusTraversable(false);
+		
+		
+		
+		userElementsList.getItems().add(line);
+		
+	}
+
 	private boolean isValidUser() {
-		//TODO
-		return false;
+		if (! surnameField.getText().matches("([A-ZÆØÅ][a-zæøå]+)(\\-[A-ZÆØÅ][a-zæøå])?([a-zæøå]*)")) {
+			return false;
+		}
+		if (! nameField.getText().matches("([A-ZÆØÅ][a-zæøå]+)(\\-[A-ZÆØÅ][a-zæøå])?([a-zæøå]*)")) {
+			return false;
+		}
+		if (! emailField.getText().matches("[\\w_\\.æøåÆØÅ]+@[\\w_\\.æøåÆØÅ]+\\.[a-z]{2,3}")) {
+			return false;
+		}
+		String usrnm = usernameField.getText();
+		if (usrnm.length() < 5 || ! usrnm.matches("[a-zæøå]+")) {
+			return false;
+		}
+		String srnm = surnameField.getText();
+		String nm = nameField.getText();
+		String eml = emailField.getText();
+		for (User user : userList) {
+			if (usrnm.equals(user.getUsername())) {
+				return false;
+			}
+			if (srnm.equals(user.getSurname()) && nm.equals(user.getName()) && eml.equals(user.getEmail())) {
+				return false;
+			}
+		}
+		if (passwordField.getText().length() < 6) {
+			return false;
+		}
+		return true;
 	}
 
 	private void setUserList() {
@@ -154,9 +205,8 @@ public class AdminUserController implements ControllerInterface {
 				JSONObject userObj = (JSONObject) resArray.get(i);
 				user.fromJSON(userObj);
 				userList.add(user);
-				//TODO add to listView, incl. delete and change password
 			}
-			
+
 		} catch (IOException e) {
 		}
 		createUserList();
@@ -167,6 +217,7 @@ public class AdminUserController implements ControllerInterface {
 		if (calendar != null) {
 			this.calendar = calendar;
 			setUserList();
+			roomsPaneController.setData(calendar);
 		}
 	}
 
@@ -186,9 +237,26 @@ public class AdminUserController implements ControllerInterface {
 
 	@Override
 	public void setFeedback() {
-
+		delUser();
 	}
 	
+	private HBox delLine;
+	
+	private void delUser() {
+		int index =userElementsList.getItems().indexOf(delLine);
+		User delUser = userList.get(index);
+		JSONObject obj = new JSONObject();
+		obj.put("command", "delete");
+		obj.put("uid", delUser.getId());
+		try {
+			API.call("/user", obj, calendar.getSession());
+			userList.remove(index);
+			userElementsList.getItems().remove(delLine);
+		} catch (IOException e1) {
+			sceneHandler.popUpMessage("/messages/Error.fxml", 290, 140, "Something went wrong. Please try again.", new AdminUserController());
+		}
+	}
+
 	@FXML
 	private void keyPressed(KeyEvent e) {
 		if (e.getCode()==KeyCode.ESCAPE) {
