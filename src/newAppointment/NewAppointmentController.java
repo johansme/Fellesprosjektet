7f1a1d9a594d39b7db7b1783727ad7e5dcc4f1b6
Alexander Bakken <alex.h.bakken@gmail.com>
2015-03-20 11:26:43 +0100

    fix appointment time bug

--- src/newAppointment/NewAppointmentController.java
+++ src/newAppointment/NewAppointmentController.java
@@ -308,6 +308,15 @@ public class NewAppointmentController implements ControllerInterface, Participan
 			
 			setRoomList();
 			
+			if(fromTimeHour > toTimeHour)
+			{
+				if(fromTimeMin > toTimeMin)
+				{
+					fromField.setText(getRoundHalfHour(toTimeHour, toTimeMin - 60));
+				}
+				else fromField.setText(getRoundHalfHour(toTimeHour, toTimeMin - 60));
+			}
+			
 			if(!validToTime()){
 				if(timeDiffInMin < 30){
 					toField.setText(getRoundHalfHour(fromTimeHour, fromTimeMin));
@@ -325,7 +334,7 @@ public class NewAppointmentController implements ControllerInterface, Participan
 				}
 			}
 
-
+			
 			if(minAndMaxAllowedStartTime())
 			{
 				if(timeDiffInMin < 30) {
